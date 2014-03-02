ad3ba6550cf4805b93e53da644a78b56d1e79219
astro2linus <astro2linus@gmail.com>
2014-03-03 01:02:05 +0800

    Initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hummingbird::Application.config.secret_key_base = '3db06b6c322a93b045921463fb0348394765844e902fe9565351ab0c6f324466bef7404fdb7c8319041506d2c0195884419e5ea0ea572963f11391bdf4f9319f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hummingbird::Application.config.session_store :cookie_store, key: '_hummingbird_session'

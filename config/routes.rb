a9a2bf9e5279f9e40c0eb76637d13cedebb6e6ab
astro2linus <astro2linus@gmail.com>
2014-03-03 05:22:02 +0800

    set root to projects#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Hummingbird::Application.routes.draw do
   resources :projects
+  root 'projects#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

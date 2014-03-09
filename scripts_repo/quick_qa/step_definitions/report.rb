71ab36fbfc19fa44304a8935f5b53dc06dabf884
astro2linus <astro2linus@gmail.com>
2014-03-09 08:57:56 +0800

    quickqa steps report.rb

---
+++ scripts_repo/quick_qa/step_definitions/report.rb
@@ -0,0 +1,19 @@
+Given(/^I have an outdated test report$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I open QuickQA project$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I open Reports page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should be able to delete the report$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see the success message$/) do
+  pending # express the regexp above with the code you wish you had
+end

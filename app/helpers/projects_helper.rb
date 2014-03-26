0223c57e4f10ebe4856f8a7f3e1f3faf1d48ac9b
astro2linus <astro2linus@gmail.com>
2014-03-26 22:13:48 +0800

    display features in a sub-directory

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -6,6 +6,9 @@ module ProjectsHelper
 	def get_project_feature_files
 		project_repo_name = @project.name.downcase.sub(' ','_')
 		path = Rails.root.join("scripts_repo/#{project_repo_name}/features/*.feature")
-		Dir.glob(path)
+		files = Dir.glob(path)
+		# also include the feature files in a sub-directory
+		sub_directory = Rails.root.join("scripts_repo/#{project_repo_name}/features/**/*.feature")
+		files += Dir.glob(sub_directory)
 	end
 end

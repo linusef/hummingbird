9c063798bd4f58372cad1079239607f8dc88e6e8
Linus Liu <linus@KiMac.local>
2014-04-16 15:16:50 +0800

    fix the issue when the project name has muptiple spaces

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -4,7 +4,7 @@ require 'multi_json'
 
 module ProjectsHelper
 	def get_project_feature_files
-		project_repo_name = @project.name.downcase.sub(' ','_')
+		project_repo_name = @project.name.downcase.gsub(' ','_')
 		path = Rails.root.join("scripts_repo/#{project_repo_name}/features/**/*.feature")
 		files = Dir.glob(path)
 		files.uniq
